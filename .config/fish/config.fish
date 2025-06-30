d8dec6914dc18a330e713558b941cdd2ff50dc41
Cameron Kleiman <cameron.kleiman@datadoghq.com>
2025-06-30 14:19:25 +0000

    updates from camk

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+fish_add_path -m ~/.local/bin
 thefuck --alias | source
 COMPLETE=fish jj | source
