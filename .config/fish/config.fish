795fd3091e6fff34732b55a45bf78eb39331b5c6
Cameron Kleiman <cameron.kleiman@datadoghq.com>
2025-12-11 18:45:52 +0000

    new jj branch syntax and other fish config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,5 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fish_add_path -m ~/.local/bin
 thefuck --alias | source
 ddr completion fish | source
+direnv hook fish | source
 COMPLETE=fish jj | source
