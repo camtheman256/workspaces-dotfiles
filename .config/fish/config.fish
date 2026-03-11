39a40c6f76e5624f6f98757c7a52fbda680f0004
Cameron Kleiman <cameron.kleiman@datadoghq.com>
2026-03-11 12:36:45 +0000

    frontend changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,8 @@ fish_add_path -m ~/.local/bin
 ddr completion fish | source
 direnv hook fish | source
 COMPLETE=fish jj | source
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Added by Yarn Switch
+source "/home/bits/.yarn/switch/env.fish"
